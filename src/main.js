248084ca17779ab45459a9c189b5f1099e30bcf0
mohamed abd elmoniem <mohammed.abd.elmoniiem@gmail.com>
2025-11-19 21:29:36 +0200

    createCubes function

--- src/main.js
+++ src/main.js
@@ -1,9 +1,24 @@
 import './style.css';
-
+import GUI from 'lil-gui';
+import Stats from 'stats.js';
 import RAPIER from '@dimforge/rapier3d-compat';
-import * as THREE from 'three'
+import * as THREE from 'three';
+import { EffectComposer, GlitchPass, OrbitControls, RenderPass, SMAAPass, TAARenderPass } from 'three/examples/jsm/Addons.js';
+import { update } from 'three/examples/jsm/libs/tween.module.js';
 
-import { OrbitControls } from 'three/examples/jsm/Addons.js';
+
+
+
+const gui = new GUI()
+const stats = new Stats();
+stats.showPanel(0)
+document.body.appendChild(stats.dom)
+
+
+
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++
 
   const canvasElement = document.querySelector('canvas');
   console.log(THREE);
@@ -14,6 +29,8 @@ import { OrbitControls } from 'three/examples/jsm/Addons.js';
   };
 
 async function init(){
+
+
 
 
   // physics 
@@ -31,7 +48,7 @@ async function init(){
 
   const camera = new THREE.PerspectiveCamera(75,canvasSize.aspect(),0.1,1000);
   camera.position.z = 5;
-  camera.position.y = 5;
+  camera.position.y = 15;
 
   scene.add(camera)
 
@@ -39,34 +56,119 @@ async function init(){
   const renderer = new THREE.WebGLRenderer({canvas:canvasElement , antialias:true});
   renderer.render(scene , camera);
   renderer.setSize(canvasSize.width(), canvasSize.height());
+  // renderer.shadowMap.enabled = true;
+  // renderer.shadowMap.type = THREE.VSMShadowMap;
+
+
+
+  // postprocessing ++++++++++++++++++++
+  // const postprocessingFoder = gui.addFolder('postprocessing')
+  // const effectComposser = new EffectComposer(renderer);
+
+
+  // const renderPass= new RenderPass(scene,camera);
+  // effectComposser.addPass(renderPass)
+
+  // // const ray = new GodRay
+
+  // const glitchPass = new GlitchPass()
+  // effectComposser.addPass(glitchPass);
+
+  // postprocessingFoder.add(glitchPass,'enabled').name('glitch enable');
+
+  // const smaaPass = new SMAAPass();
+  // smaaPass.setSize(canvasSize.width(),canvasSize.height())
+  // effectComposser.addPass(smaaPass);
+  // console.log(smaaPass)
+  // postprocessingFoder.add(smaaPass,'enabled').name('enable smaa')
+  // postprocessingFoder.add(smaaPass.,'samples').name('enable smaa')
+
+  // const taaPass = new TAARenderPass(scene,camera);
+  // effectComposser.addPass(taaPass)
+
+  // postprocessingFoder.add(taaPass,'enabled').name('taa Pass enable')
+  // console.log(taaPass)
+
+
+
+
+
+
+
   // -------------------------------------------------------------------------end of renderer
 
 
 
   // objects +++++++++++++++++++++++++++++++
-  const box = new THREE.Mesh(
-    new THREE.BoxGeometry(1,1,1),
-    new THREE.MeshBasicMaterial({color:0xff00ff})
-  )
 
-  scene.add(box);
+  const cubesProperties ={
+    count:5,
+  }
 
-  const boxBody = world.createRigidBody(RAPIER.RigidBodyDesc.dynamic().setTranslation(0,5,0));
-  world.createCollider(RAPIER.ColliderDesc.cuboid(0.5,0.5,0.5),boxBody)
+  // const box = new THREE.Mesh(
+  //   new THREE.BoxGeometry(1,1,1),
+  //   new THREE.MeshPhongMaterial({color:0xff00ff})
+  // )
+  // box.castShadow  =true
+  // const boxWire = new THREE.Mesh(
+  //   new THREE.BoxGeometry(1,1,1),
+  //   new THREE.MeshBasicMaterial({color:0x00000f ,wireframe:true})
+  // )
+  //  box.add(boxWire)
+  // scene.add(box);
+
+  // const boxBody = world.createRigidBody(RAPIER.RigidBodyDesc.dynamic().setTranslation(0,5,0));
+  // const boxCollider = RAPIER.ColliderDesc.cuboid(0.5,0.5,0.5);
+  // boxCollider.restitution = 0.8
+
+  // world.createCollider(boxCollider,boxBody)
+
+
+ 
+  let randomCubes = createBoxes();
+  scene.add(...randomCubes.map(ele=>ele.mesh));
+
+
+   const cubesControls = gui.addFolder('cubes Properties');
+   cubesControls.add(cubesProperties,'count',1,1000,5).name('number of cubes').onChange( value=>reCreateCubes(value))
+   cubesControls.add({restart:false},'restart').onChange(()=>reCreateCubes(cubesProperties.count));
+
+
+  //  pushing cubes
+
+  const moverMat = new THREE.MeshPhysicalMaterial({color:0x999999});
+  const moverMatWire = new THREE.MeshPhongMaterial({color:0xff0022,emissive:0xffffff,wireframe:true});
+
+  const moverGeo = new THREE.BoxGeometry(3,3,3,6,6,6);
+
+  const mover = new THREE.Mesh(moverGeo,moverMat);
+  const moverWire = new THREE.Mesh(moverGeo,moverMatWire);
+
+  mover.add(moverWire)
+ 
+
+  scene.add(mover);
+
+  const moverBody = world.createRigidBody( RAPIER.RigidBodyDesc.dynamic().setTranslation(1.5,1.55,1.5));
+  world.createCollider(RAPIER.ColliderDesc.cuboid(1.5,1.5,1.5).setFriction(0.5).setMass(40),moverBody)
+
+
 
 
   // ground ++++++++++
+  const length = 200;
   const ground = new THREE.Mesh(
-    new THREE.PlaneGeometry(30,30),
-    new THREE.MeshBasicMaterial({color:0xffffff ,side:THREE.DoubleSide})
+    new THREE.PlaneGeometry(length,length),
+    new THREE.MeshPhongMaterial({color:0xffffff ,side:THREE.DoubleSide})
   )
+  ground.receiveShadow =true;
   ground.rotation.x = Math.PI * 0.5
 
   scene.add(ground);
 
   const groundBody =  world.createRigidBody(RAPIER.RigidBodyDesc.fixed().setTranslation(0,0,0));
   console.log(groundBody)
-  world.createCollider(RAPIER.ColliderDesc.cuboid(15,0.1,15),groundBody)
+  world.createCollider(RAPIER.ColliderDesc.cuboid(length*0.5,0.1,length*0.5).setFriction(0.5),groundBody)
 
 
 
@@ -75,30 +177,160 @@ async function init(){
   const control = new OrbitControls(camera,canvasElement)
   control.update()
 
-
+//  console.log(camera)
   // animation ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  const clock = new THREE.Clock()
+  let time = 0
 
   function animate(){
 
+
+    stats.begin();
+
+    time = clock.getElapsedTime()
+
+
+
+
+
+
     renderer.render(scene,camera);
+    // effectComposser.render(clock.getDelta())
     world.step()
     // console.log(boxBody.translation())
-    box.position.copy(boxBody.translation())
+    mover.position.copy(moverBody.translation());
+    mover.quaternion.copy(moverBody.rotation());
+    // moverBody.setRotation.y =mover.quaternion.y
+
+    randomCubes.forEach(ele=>ele.update())
+
+
+    // move the camera on a circle path manually
+    // camera.position.x = 15 * Math.cos(time * 0.4);
+    // camera.position.z = 15 * Math.sin(time * 0.4);
+    // camera.lookAt(mover.position)
+
+   
+
+
+
+
+
+    stats.end()
     requestAnimationFrame( animate );
   }
 
   animate()
 
 
+// lights+++++++++++++++++++++++++++++++++++++++++++
+
+const pointLight = new THREE.PointLight(0xffeaa3 , 4,30,0.1);
+pointLight.position.set(5,15,0);
+pointLight.castShadow = true;
+// pointLight.shadow.mapSize.set(512,512);
+// pointLight.shadow.bias = 0.005;
+// pointLight.shadow.normalBias = 0.05;
+// pointLight.shadow.radius = 100
+
+const amLight = new THREE.AmbientLight(0x555555,0.2)
 
 
 
-  console.log(canvasElement,canvasSize.aspect())
+// scene.add(pointLight,amLight);
+//  --------------------------------------------------------------lights
 
-  console.log(RAPIER)
 
 
+
+// functions +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+function reCreateCubes(count){
+  randomCubes.forEach(ele=>{
+
+        // remove from scene
+        scene.remove(ele.mesh)
+        ele.mesh.geometry.dispose();
+        // remove from the world
+        world.removeRigidBody(ele.rigidBody);
+
+        
+      })
+      
+      // / create new cubes
+
+        randomCubes = createBoxes(count);
+        scene.add(...randomCubes.map(ele=>ele.mesh))
+}
+
+function createBoxes(count = 5){
+
+  const boxMat = new THREE.MeshPhongMaterial({color:0xff00ff});
+  const wireMat =  new THREE.MeshStandardMaterial({color:0x22eeff,wireframe:true,roughness:0.2,metalness:0.1,emissive:0xff00ff,emissiveIntensity:3});
+  
+  const boxGeo =  new THREE.BoxGeometry(1,1,1);
+  const Cubes = []
+
+  for(let i = 0 ; i< count ; i++){
+ 
+   const box = new THREE.Mesh(boxGeo,boxMat);
+  box.castShadow  =true
+  const boxWire =new THREE.Mesh(boxGeo,wireMat);
+  boxWire.material.wireframe =true
+   box.add(boxWire)
+  
+
+  const boxBody = world.createRigidBody(RAPIER.RigidBodyDesc.dynamic().setTranslation(10*(Math.random()-0.5),15+50*Math.random() + 5,10*(Math.random()-0.5)));
+  const boxCollider = RAPIER.ColliderDesc.cuboid(0.5,0.5,0.5);
+  boxCollider.restitution = 0.4
+
+  world.createCollider(boxCollider,boxBody);
+  
+  Cubes.push({mesh:box , rigidBody:boxBody,update:connectMeshToRigid})
+  }
+
+  return Cubes
+
+ 
+}
+
+function connectMeshToRigid(){
+  this.mesh.position.copy(this.rigidBody.translation());
+  this.mesh.quaternion.copy(this.rigidBody.rotation());
+}
+
+
+
+
+// control the mover
+window.addEventListener('keydown',eve=>{
+  console.log(eve.key)
+  if(eve.key =='ArrowUp'){
+  
+    moverBody.applyImpulse({x:0,y:0,z:20})
+  }
+  if(eve.key =='ArrowDown'){
+    
+    moverBody.applyImpulse({x:0,y:0,z:-20})
+  }
+   if(eve.key =='ArrowRight'){
+     moverBody.applyImpulse({x:20,y:0,z:0})
+
+  }
+   if(eve.key =='ArrowLeft'){
+   
+    moverBody.applyImpulse({x:-20,y:0,z:0})
+  }
+
+  
+})
 
 }
 
-init()
+init();
+
+
+
+
+  
+
